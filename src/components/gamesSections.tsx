72ed590cc504813e40251eaad0336250f9029220
keseyf <keseyf.exe@proton.me>
2025-03-17 20:04:58 -0300

    fixing path error

--- src/components/gamesSections.tsx
+++ src/components/gamesSections.tsx
@@ -4,7 +4,7 @@ function HouseGames() {
     return (
         <article className="flex flex-col w-full space-y-1 justify-center items-center">
             <div className="flex flex-wrap gap-5 p-10 w-full">
-                <GameCard inputFile="/public/gamedicelogo.png" nameGame="Dice" url="/games/underover7" canplay={true} />
+                <GameCard inputFile="public/gamedicelogo.png" nameGame="Dice" url="/games/underover7" canplay={true} />
             </div>
 
         </article>
